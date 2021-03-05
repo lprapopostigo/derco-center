2da4d61c6366a7b567443c8b20b11f927077fad2
Luis Prado <lpradopostigo@gmail.com>
2021-03-04 22:26:20 -0500

    animation added

--- slider.js
+++ slider.js
@@ -1,1 +1,20 @@
-const slider = document.querySelectorAll('#slider');
+const slider = document.querySelectorAll('#slider img');
+
+slider.forEach((e) => {
+  e.style.opacity = 0;
+});
+slider[0].style.opacity = 100;
+
+let i = 0;
+
+window.setInterval(() => {
+  if (i === slider.length - 1) {
+    i = 0;
+  } else {
+    i += 1;
+  }
+  slider.forEach((e) => {
+    e.style.opacity = 0;
+  });
+  slider[i].style.opacity = 100;
+}, 5000);
